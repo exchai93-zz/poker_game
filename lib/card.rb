7dedc65dc22d585f6cc3daa72438083fb20fa16a
Emily Chai <echai93@gmail.com>
2017-07-14 11:45:20 +0100

    Define card values and ranks in a hash

---
+++ lib/card.rb
@@ -0,0 +1,35 @@
+class Card
+
+  # hash of card values
+  CARD_VALUES = {
+    deuce: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+    ten: "10",
+    jack: "J",
+    queen: "Q",
+    king: "K",
+    ace: "A"
+  }
+
+  # hash of rank of each card
+  POKER_RANK = {
+    deuce: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    ten: 10,
+    jack: 11,
+    queen: 12,
+    king: 13,
+    ace: 1
+  }
