1c8232170b04547815ffd4221eab6bcfd08a15e8
Emily Chai <echai93@gmail.com>
2017-07-18 09:17:17 +0100

    WIP get number of players by user input

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,26 @@
 class Player
-end 
+
+  DEFAULT_NUMBERS = ["2", "3", "4", "5"]
+
+  attr_reader :players
+
+  def initialize(input_number)
+    @players = input_number
+  end
+
+  def get_player_number(input)
+    "#{input} players"
+  end
+
+  player = Player.new(2)
+
+  puts "How many players are there? (Choose between 2-5)"
+  players = gets.chomp.to_i
+
+  puts get_player_number
+
+  private
+
+  attr_writer :players
+
+end
