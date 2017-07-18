15788ceba65037b15495c5ea08f5371d31e277b0
Emily Chai <echai93@gmail.com>
2017-07-18 09:16:20 +0100

    WIP card is initialized with a value parameter

--- lib/card.rb
+++ lib/card.rb
@@ -37,9 +37,13 @@ class Card
   # method to read attributes
   attr_reader :value
 
-  # card class is initialized with a value paramter and attribute
+  # card class is initialized with a value parameter and attribute
   def initialize(value)
     @value = value
   end
 
-end 
+  private
+
+  attr_writer :value
+
+end
