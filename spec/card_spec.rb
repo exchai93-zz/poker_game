7963352aa00b51468556cfa6737bea24c189897e
Emily Chai <echai93@gmail.com>
2017-07-18 09:17:43 +0100

    Change let(:card) to subject(:card)

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,8 +2,7 @@ require 'card'
 
 describe Card do
 
-  let(:card) {described_class.new("A")}
-  # subject(:card) {described_class.new(value)}
+  subject(:card) {described_class.new("A")}
 
   it 'initializes card with a value' do
     expect(card.value).to eq "A"
