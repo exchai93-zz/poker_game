00f0c797ef86ffb6241ee448fd66cb427d103085
Emily Chai <echai93@gmail.com>
2017-07-18 15:03:45 +0100

    Extractcard rank and card value modules

--- lib/card.rb
+++ lib/card.rb
@@ -1,49 +1,13 @@
+require_relative 'card_values'
+
 class Card
-
-  # hash of card values
-  CARD_VALUES = {
-    deuce: "2",
-    three: "3",
-    four: "4",
-    five: "5",
-    six: "6",
-    seven: "7",
-    eight: "8",
-    nine: "9",
-    ten: "10",
-    jack: "J",
-    queen: "Q",
-    king: "K",
-    ace: "A"
-  }
-
-  # hash of rank of each card
-  POKER_RANK = {
-    deuce: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-    ten: 10,
-    jack: 11,
-    queen: 12,
-    king: 13,
-    ace: 1
-  }
-
+  # require CardValues module
+  include CardValues
   # method to read attributes
   attr_reader :value
-
   # card class is initialized with a value parameter and attribute
   def initialize(value)
     @value = value
   end
-
-  private
-
-  attr_writer :value
 
 end
---
+++ lib/card_values.rb
@@ -0,0 +1,20 @@
+module CardValues
+
+# hash of card values
+CARD_VALUES = {
+  deuce: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+  ten: "10",
+  jack: "J",
+  queen: "Q",
+  king: "K",
+  ace: "A"
+}
+
+end 
