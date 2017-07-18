dd41a36ddd63932358c923e655c843a819ac2293
Emily Chai <echai93@gmail.com>
2017-07-18 16:39:30 +0100

    create_deck method, test passes

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,9 +6,24 @@ class Deck
 
   attr_reader :full_deck
   # Deck class is initialized with an empty array named full_deck
-  def initialize( full_deck = [])
+  def initialize(full_deck = [])
     @full_deck = full_deck
   end
 
+  # Method to create a new deck of cards
+   def create_deck
+     @full_deck = []
+     # From the CARD_SUITS hash, retrieve the keys
+     suits = CARD_SUITS.keys
+     # From the CARD_VALUES hash, retrieve the values
+     values = CARD_VALUES.values
+     # Iterate over each suit and value then push them to the full_deck array
+     suits.each do |suit|
+       values.each do |value|
+         full_deck << Card.new(value, suit)
+       end
+     end
+     Deck.new(full_deck)
+   end
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,9 +5,14 @@ describe Deck do
   subject(:deck) {described_class.new}
   let(:deck_2) {described_class.new}
 
-
   it 'initializes with an empty array' do
     expect(deck.full_deck).to eq []
+  end
+
+  describe '#create_deck' do
+    it 'should have 52 cards in a full deck' do
+      expect(deck.deck_count).to eq 52
+    end
   end
 
 end
