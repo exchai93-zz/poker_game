0afaf15536cabdb1729c03c933c083c33daacc4a
Emily Chai <echai93@gmail.com>
2017-07-18 09:21:51 +0100

    WIP first test to complete

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,4 +2,10 @@ require 'player'
 
 describe Player do
 
+  subject(:player) {described_class.new}
+
+  it 'initializes with a number of players' do
+    expect(player.input_number).to eq input_number
+  end
+  
 end
