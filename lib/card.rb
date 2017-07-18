7bc53c632123fe4df5e7d959e0985fd74262b075
Emily Chai <echai93@gmail.com>
2017-07-18 16:34:03 +0100

    Add suit to attr_reader

--- lib/card.rb
+++ lib/card.rb
@@ -4,7 +4,7 @@ class Card
   # require CardValues module
   include CardValues
   # method to read attributes
-  attr_reader :value
+  attr_reader :value, :suit
   # card class is initialized with a value parameter and attribute
   def initialize(value, suit)
     @value = value
