cf4acfaf02088ce581206789c86d190fd97a2d7d
Emily Chai <echai93@gmail.com>
2017-07-19 10:54:01 +0100

    WIP includ CardRanks module

--- lib/player.rb
+++ lib/player.rb
@@ -1,26 +1,32 @@
+require_relative 'hand'
+
 class Player
 
-  DEFAULT_NUMBERS = ["2", "3", "4", "5"]
+  include CardRanks
+
+  DEFAULT_NUMBER_OF_PLAYERS = ["2", "3", "4", "5"]
 
   attr_reader :players
 
-  def initialize(input_number)
-    @players = input_number
+  def initialize(hand = Hand.new, score = 0, player_number = 0)
+    @player_number = player_number
+    @hand = hand
+    @score = score
   end
 
-  def get_player_number(input)
-    "#{input} players"
-  end
+  # def get_player_number
+  #   puts "How many players are there? (Choose between 2, 3, 4 and 5)"
+  #   players = gets.chomp.to_i
+  #   players
+  # end
 
-  player = Player.new(2)
+  # if players == DEFAULT_NUMBER_OF_PLAYERS
 
-  puts "How many players are there? (Choose between 2-5)"
-  players = gets.chomp.to_i
 
-  puts get_player_number
-
-  private
-
-  attr_writer :players
+  # puts get_player_number
+  #
+  # private
+  #
+  # attr_writer :players
 
 end
