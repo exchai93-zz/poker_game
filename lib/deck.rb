4f77ad06c246995fe944e55a657c8e07b0d1b906
Emily Chai <echai93@gmail.com>
2017-07-19 16:22:59 +0100

    WIP to correct specs, implement #deal_card

--- lib/deck.rb
+++ lib/deck.rb
@@ -10,29 +10,41 @@ class Deck
     @full_deck = full_deck
   end
 
-  # Method to create a new deck of cards
-   def create_deck
-     @full_deck = []
-     # From the CARD_SUITS hash, retrieve the keys
-     suits = CARD_SUITS.keys
-     # From the CARD_VALUES hash, retrieve the values
-     values = CARD_VALUES.values
-     # Iterate over each suit and value then push them to the full_deck array
-     suits.each do |suit|
-       values.each do |value|
-         full_deck << Card.new(value, suit)
-       end
-     end
-     Deck.new(full_deck)
+  # Create a new full deck of cards
+  def create_deck
+    @full_deck = []
+    # From the CARD_SUITS hash, retrieve the keys
+    suits = CARD_SUITS.keys
+    # From the CARD_VALUES hash, retrieve the values
+    values = CARD_VALUES.values
+    # Iterate over each suit and value then push them to the full_deck array
+    suits.each do |suit|
+      values.each do |value|
+        full_deck << Card.new(value, suit)
+      end
+    end
+      Deck.new(full_deck)
    end
-
-   def deck_count
-     create_deck
-     @full_deck.length
+   # All the elements in the full_deck array are shuffled
+   def shuffle!
+     @full_deck.shuffle!
+   end
+  # A random card from the deck is returned
+  def deal_card
+    card = get_random_card_from_deck
+    remove_card_from_deck(card)
   end
 
-  def shuffle!
-    @full_deck.shuffle!
+  def deck_count
+    @full_deck.length
+  end
+
+  def get_random_card_from_deck
+    @full_deck.sample
+  end
+
+  def remove_card_from_deck(card)
+    @full_deck.delete(card)
   end
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,9 +1,11 @@
 require 'deck'
+require 'card'
 
 describe Deck do
 
   subject(:deck) {described_class.new}
   let(:deck_2) {described_class.new}
+  let(:card) {Card.new("A", "hearts")}
 
   it 'initializes with an empty array' do
     expect(deck.full_deck).to eq []
@@ -11,6 +13,7 @@ describe Deck do
 
   describe '#create_deck' do
     it 'should have 52 cards in a full deck' do
+      deck.create_deck
       expect(deck.deck_count).to eq 52
     end
   end
@@ -21,5 +24,19 @@ describe Deck do
       expect(deck).not_to eq deck_2
     end
   end
-  
+
+  describe '#deal_card' do
+    before do
+      deck.create_deck
+      deck.shuffle!
+    end
+    it 'should return one random card' do
+      expect(deck.deal_card).to eq card
+    end
+    it 'should decrease the full deck by one' do
+      deck.deal_card
+      expect(deck.deck_count).to eq 51
+    end
+  end
+
 end
