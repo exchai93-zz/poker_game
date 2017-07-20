ab5f6da841fd9f1a5ca90119bbfbbca0bac8cfe6
Emily Chai <echai93@gmail.com>
2017-07-20 10:10:03 +0100

    player#get_player_number gets user input

--- lib/player.rb
+++ lib/player.rb
@@ -4,9 +4,9 @@ class Player
 
   include CardRanks
 
-  DEFAULT_NUMBER_OF_PLAYERS = ["2", "3", "4", "5"]
+  CARDS_IN_HAND = 5
 
-  attr_reader :players
+  attr_reader :player_number, :hand, :score
 
   def initialize(hand = Hand.new, score = 0, player_number = 0)
     @player_number = player_number
@@ -14,19 +14,21 @@ class Player
     @score = score
   end
 
-  # def get_player_number
-  #   puts "How many players are there? (Choose between 2, 3, 4 and 5)"
-  #   players = gets.chomp.to_i
-  #   players
-  # end
+  def get_player_number
+    while true
+      puts "How many players are there? Choose between 2, 3, 4 and 5"
+      input = gets.chomp.to_i
+      break if (input == 2 || input == 3 || input == 4 || input == 5)
+        puts "Oops. Please input a valid number of players."
+    end
+  end
 
-  # if players == DEFAULT_NUMBER_OF_PLAYERS
+  private
 
-
-  # puts get_player_number
-  #
-  # private
-  #
-  # attr_writer :players
+  attr_writer :player_number 
 
 end
+
+player = Player.new
+
+player.get_player_number
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,27 @@
 require 'player'
+require 'hand'
 
 describe Player do
 
+  let(:hand) {Hand.new}
   subject(:player) {described_class.new}
 
-  it 'initializes with a number of players' do
-    expect(player.input_number).to eq input_number
+  describe '#initialize' do
+    it 'has a player number of 0' do
+      expect(player.player_number).to eq 0
+    end
+    it 'has an empty hand of cards' do
+      expect(player.hand).to eq hand
+    end
+    it 'has a score of 0' do
+      expect(player.score).to eq 0
+    end
   end
-  
+
+  describe '#get_player_number' do
+    it 'gets a user input between 2 - 5' do
+      expect(player.get_player_number).to eq 3
+    end
+  end
+
 end
