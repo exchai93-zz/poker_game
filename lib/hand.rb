f830db545940372fad298295a4695e40580930b8
Emily Chai <echai93@gmail.com>
2017-07-25 17:24:38 +0100

    #display_cards_in_hand converts string of card values and suits to single array

--- lib/hand.rb
+++ lib/hand.rb
@@ -13,16 +13,29 @@ class Hand
     @hand_value = hand_value
   end
 
+  def draw(card)
+   @cards << card
+   if hand_is_full?
+     get_hand_value
+   end
+  end
+
   def get_hand_value
   values = []
   @cards.to_h.keys.each do |card|
     values << Hand::FACE_VALUES[card]
   end
   values.sum
-end
+  end
 
   def hand_is_full?
    @cards.length == NUM_OF_CARDS_IN_HAND
- end
+  end
+
+  def display_cards_in_hand
+   str = ""
+   @cards.flatten.each { |card| str += "#{card.to_s}" } #try and take out of array? into just string
+   str.scan(/../)
+  end
 
 end
