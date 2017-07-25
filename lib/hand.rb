35bacf516c6e5da8458088ced54c68d0b91f506c
Emily Chai <echai93@gmail.com>
2017-07-25 17:20:47 +0100

    Alter hand class with CardProperties module and hand_value instance variable

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,20 +1,16 @@
-require_relative 'deck'
-require_relative 'card_ranks'
+require_relative 'dealer'
+require_relative 'card_properties'
+
+NUM_OF_CARDS_IN_HAND = 5
 
 class Hand
+  include CardProperties
 
-  include CardValues
-  include CardRanks
-
-  attr_reader :cards
+  attr_reader :cards, :hand_value
 
   def initialize
     @cards = []
+    @hand_value = hand_value
   end
-
-  # def rank
-  #   rank = CARD_RANK.values
-  #   rank
-  # end
 
 end
