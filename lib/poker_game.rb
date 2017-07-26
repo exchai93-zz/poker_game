dee9d467f7be4efe259b86ac87ae5facfe99cf63
Emily Chai <echai93@gmail.com>
2017-07-26 20:52:22 +0100

    #play to initialize each player with a hand

--- lib/poker_game.rb
+++ lib/poker_game.rb
@@ -1,40 +1,32 @@
-require_relative 'player'
 require_relative 'deck'
 require_relative 'dealer'
+require_relative 'player'
 
 class PokerGame
 
-  attr_reader :players, :current_player, :deck
+  attr_reader :players, :deck, :dealer
 
-  def initialize(players = [Player.new], dealer = Dealer.new)
-    @players = players
-    @dealer = dealer
+  def initialize
+    @players = []
     @deck = Deck.create_deck
+    @dealer = Dealer.new
   end
 
-  def deal_cards
+  def play
     number_players = 0
-    cards_per_player = 0
     until number_players >= 2 && number_players <= 5
-      puts "How many players are there? Choose between 2,3,4 and 5."
+      puts "How many players are there? Choose between 2, 3, 4 and 5."
       number_players = gets.chomp.to_i
+      puts "Ok let's play.\nDealing five cards to #{number_players} players..."
     end
 
-    max_cards_in_hand = 52 / number_players
-    until cards_per_player >= 1 and cards_per_player <= max_cards_in_hand
-      puts "How many cards per player? (Choose up to #{max_cards_in_hand} cards)"
-      cards_per_player = gets.chomp.to_i
+    # Initialize each player with a hand
+    for player_number in  1..number_players
+      @players << Player.new(Hand.new, 0, player_number)
     end
-    puts "Ok let's play.\nDealing #{cards_per_player} cards to #{number_players} players."
 
-    deck = @deck.shuffle!
-    hands = []
-    (0...number_players).each do |i|
-      @hands[i] = deck.shift(cards_per_player)
-    end
-  end
 
 end
-
+# run game: ruby poker_game.rb
 game = PokerGame.new
-game.deal_cards
+game.play
