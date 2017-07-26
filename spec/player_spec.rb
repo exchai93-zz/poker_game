c9c853754946d8fbf8f0c764b5bffa419563b24e
Emily Chai <echai93@gmail.com>
2017-07-26 20:11:29 +0100

    Refactor tests, all pass

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,24 +3,18 @@ require 'hand'
 
 describe Player do
 
-  let(:hand) {Hand.new}
-  subject(:player) {described_class.new}
+  let(:hand) { double(Hand.new) }
+  subject(:player) { described_class.new }
 
   describe '#initialize' do
-    it 'has a player number of 0' do
+    it 'has a player number set to 0' do
       expect(player.player_number).to eq 0
     end
     it 'has an empty hand of cards' do
-      expect(player.hand).to eq hand
+      expect(player.hand.cards).to be_empty
     end
-    it 'has a score of 0' do
+    it 'has a score set to 0' do
       expect(player.score).to eq 0
-    end
-  end
-
-  describe '#get_player_number' do
-    it 'gets a user input between 2 - 5' do
-      expect(player.get_player_number).to eq 3
     end
   end
 
