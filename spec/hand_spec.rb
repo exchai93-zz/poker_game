441c899eb791a780f4731b3968838f0fd72d4679
Emily Chai <echai93@gmail.com>
2017-07-26 20:10:28 +0100

    Correct tests, all pass

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,11 +1,35 @@
 require 'hand'
+require 'pry'
 
 describe Hand do
 
-  subject(:hand) {described_class.new}
+  subject(:hand) { described_class.new }
+  let(:card) { Card.new }
 
-  it 'initializes with an empty array of cards' do
-    expect(hand.cards).to eq []
+  describe '#initialize' do
+    it 'has an empty array of cards' do
+      expect(hand.cards).to eq []
+    end
+  end
+
+  describe '#draw' do
+    it 'increases the array by one' do
+      expect{ hand.draw(card) }.to change { hand.cards.count }.by 1
+    end
+  end
+
+  describe '#get_hand_value' do
+    it 'sums the face values of full hand' do
+      hand.cards = [["5", :C], ["4", :D], ["5", :H], ["Q", :C], ["4", :C]]
+      expect(hand.get_hand_value).to eq 21
+    end
+  end
+
+  describe '#display_cards_in_hand' do
+    it 'return a single array of string of cards' do
+      hand.cards = [["5", :C], ["4", :D], ["5", :H], ["Q", :C], ["4", :C]]
+      expect(hand.display_cards_in_hand).to eq "5C, 4D, 5H, QC, 4C"
+    end
   end
 
 end
