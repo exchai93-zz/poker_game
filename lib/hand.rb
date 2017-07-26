b48d27583777d986b0850736cb186298cad6eb0f
Emily Chai <echai93@gmail.com>
2017-07-26 20:09:55 +0100

    Refactor methods and add inline comments

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,41 +1,43 @@
-require_relative 'dealer'
 require_relative 'card_properties'
-
-NUM_OF_CARDS_IN_HAND = 5
 
 class Hand
   include CardProperties
 
-  attr_reader :cards, :hand_value
+  attr_reader :hand_value
+  attr_accessor :cards
+
+  # Magic number setting the maxmimum number of cards in hand
+  NUM_OF_CARDS_IN_HAND = 5
 
   def initialize
     @cards = []
     @hand_value = hand_value
   end
 
+  # Hand draws one card, once the hand is full the value will be calculated.
   def draw(card)
    @cards << card
    if hand_is_full?
      get_hand_value
    end
   end
-
+  # Calculates the value of the hand by each face value
   def get_hand_value
-  values = []
-  @cards.to_h.keys.each do |card|
-    values << Hand::FACE_VALUES[card]
+    values = []
+    @cards.to_h.keys.each do |card|
+      values << Hand::FACE_VALUES[card]
+    end
+    values.sum
   end
-  values.sum
-  end
-
+  # Returns true if the number of cards in hand reaches 5
   def hand_is_full?
    @cards.length == NUM_OF_CARDS_IN_HAND
   end
-
+  # Shows the cards in a string format 
   def display_cards_in_hand
-   str = ""
-   @cards.flatten.each { |card| str += "#{card.to_s}" } #try and take out of array? into just string
-   str.scan(/../)
+    @cards.collect do |card|
+      card.join
+    end.join(", ")
   end
 
 end
